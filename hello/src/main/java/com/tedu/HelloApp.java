a22ed58d172c4e73aa712847d1d6826b345363c5
org-bear <17688891560@163.com>
2018-10-16 14:10:26 +0800

    test

---
+++ hello/src/main/java/com/tedu/HelloApp.java
@@ -0,0 +1,10 @@
+package com.tedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class HelloApp {
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApp.class, args);
+	}
+}
